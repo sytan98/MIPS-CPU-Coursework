5c537f9a0f7b66a20beb828e3fab3b27b7cf377a
sytan98 <tan.siyu@dhs.sg>
2020-11-23 17:21:10 +0800

    added scripts

---
+++ src/mips_cpu_bus.v
@@ -0,0 +1,16 @@
+module mips_cpu_bus(
+    /* Standard signals */
+    input logic clk,
+    input logic reset,
+    output logic active,
+    output logic[31:0] register_v0,
+
+    /* Avalon memory mapped bus controller (master) */
+    output logic[31:0] address,
+    output logic write,
+    output logic read,
+    input logic waitrequest,
+    output logic[31:0] writedata,
+    output logic[3:0] byteenable,
+    input logic[31:0] readdata
+);
