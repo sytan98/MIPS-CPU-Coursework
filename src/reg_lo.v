720df34e245f6804091dc406b955dd1d5e0769a4
ml3319 <ml3319@imperial.ac.uk>
2020-11-28 00:33:43 -0800

    reg_hi reg_lo modified + tested

--- src/reg_lo.v
+++ src/reg_lo.v
@@ -7,13 +7,16 @@ module reg_lo(
   output logic[31:0] lo_read
 );
 
-always_ff @(posedge clk) begin
-  if(lo_wren==1) begin
-    lo_read <= read_data_a;
+  always_ff @(posedge clk) begin
+    if(reset) begin
+      lo_read <= 0;
+    end
+    else if(lo_wren) begin
+      lo_read <= read_data_a;
+    end
+    else begin
+      lo_read <= lo;
+    end
   end
-  else begin
-    lo_read <= lo;
-  end
-end
 
 endmodule
