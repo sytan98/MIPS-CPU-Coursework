c0d35d1b59888d1d22414ef72f993f8e42a4e3f0
sytan98 <tan.siyu@dhs.sg>
2020-12-08 01:17:26 +0800

    new bus branch

--- rtl/mips_cpu_bus.v
+++ rtl/mips_cpu_bus.v
@@ -1,23 +1,18 @@
-module mips_cpu_harvard(
+module mips_cpu_bus(
     /* Standard signals */
-    input logic     clk,
-    input logic     reset,
-    output logic    active,
-    output logic [31:0] register_v0,
+    input logic clk,
+    input logic reset,
+    output logic active,
+    output logic[31:0] register_v0,
 
-    /* New clock enable. See below. */
-    input logic     clk_enable,
-
-    /* Combinatorial read access to instructions */
-    output logic[31:0]  instr_address,
-    input logic[31:0]   instr_readdata,
-
-    /* Combinatorial read and single-cycle write access to instructions */
-    output logic[31:0]  data_address,
-    output logic        data_write,
-    output logic        data_read,
-    output logic[31:0]  data_writedata,
-    input logic[31:0]  data_readdata
+    /* Avalon memory mapped bus controller (master) */
+    output logic[31:0] address,
+    output logic write,
+    output logic read,
+    input logic waitrequest,
+    output logic[31:0] writedata,
+    output logic[3:0] byteenable,
+    input logic[31:0] readdata
 );
 
 //cpu state
