34fdd4d8a827ceebe83730d46e59c873dc062d3c
Giorgos Vyronos <gv319@ic.ac.uk>
2020-12-10 13:13:17 +0000

    Added CPP file to obtan correct hex representation

--- assembler/C_Language_Assembler/run_c_code.sh
+++ assembler/C_Language_Assembler/run_c_code.sh
@@ -4,12 +4,14 @@ set -eo pipefail
 
 SRC="$1" #Input will be filename without extension.
 
-mipsisa32r6el-linux-gnu-gcc -O3 0-code/${SRC}.mips1.c -nostdlib -Wl,--build-id=none -T linker.ld -o 4-elf/${SRC}.mips1.elf
+mipsisa32r6el-linux-gnu-gcc -O3 0-code/${SRC}.mips1.c -nostdlib -Wl,--build-id=none -T linker.ld -o 5-elf/${SRC}.mips1.elf
 
-mipsisa32r6el-linux-gnu-objdump -d 4-elf/${SRC}.mips1.elf > 2-assembly/${SRC}.mips1.asm.txt
+mipsisa32r6el-linux-gnu-objdump -d 5-elf/${SRC}.mips1.elf > 3-assembly/${SRC}.mips1.asm.txt
 
-mipsisa32r6el-linux-gnu-objcopy -O binary --only-section=.text 4-elf/${SRC}.mips1.elf 3-binary/${SRC}.mips1.bin
+mipsisa32r6el-linux-gnu-objcopy -O binary --only-section=.text 5-elf/${SRC}.mips1.elf 4-binary/${SRC}.mips1.bin
 
-hexdump 3-binary/${SRC}.mips1.bin > 1-hex/${SRC}.mips1.hex.txt
+hexdump 4-binary/${SRC}.mips1.bin > 2-memory/${SRC}.mips1.mem.txt
 
+g++ unwrap.cpp -o unwrap
 
+./unwrap < 2-memory/${SRC}.mips1.mem.txt > 1-hex/${SRC}.mips1.hex.txt
