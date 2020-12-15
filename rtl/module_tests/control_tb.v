0f5b42c2ae47a5b998a2bdea0741b485a3b6676c
ml3319 <ml3319@imperial.ac.uk>
2020-12-15 00:10:58 -0800

    modified control testbench. testbenches for individual modules all done

--- rtl/module_tests/control_tb.v
+++ rtl/module_tests/control_tb.v
@@ -1,24 +1,181 @@
-module control_tb(
-);
-    logic[5:0] opcode;
-    logic[5:0] function_code;
-    logic[4:0] b_code; //branch instruction code
-    logic rd_select; //output - select register to write to
-    logic branch; //output - if branch, go to 1 so that block can check if condition met
-    logic imdt_sel; //output - selects which type of extended immediate value to send to alu
-    logic jump1; //output - select between output and branch mux (JAL J)
-    logic jump2; //output - select between output jump1 mux or read data a (JR JALR)
-    logic[1:0] alu_op; //output //for alu control
-    logic alu_src; //output //controls what is going into alu ()
-    logic data_read; //output //High for load instructions
-    logic data_write; //output //High for store instructions
-    logic reg_write_enable; //output //High for writing reg file (Arithmetic, logical shifts, setting inst, loading)
-    logic hi_wren; //output for writing to hi
-    logic lo_wren; //output for writing to lo
-    logic data_into_reg1; //output //
-    logic data_into_reg2; //output //for anything and links
+module control_tb();
+// inputs
+logic reset, waitrequest;
+logic[5:0] opcode;
+logic[5:0] function_code;
+logic[4:0] b_code;
+logic[2:0] state;
+logic[1:0] byte_addressing;
+
+// memory related
+logic read, write;
+logic[2:0] write_data_sel;
+logic[3:0] byteenable;
+
+// register_file related
+logic[1:0] rd_select;
+logic reg_write_enable;
+logic[2:0] datamem_to_reg;
+logic link_to_reg, mfhi, mflo, lwl, lwr;
+// hi and lo registers related
+logic hi_wren, lo_wren, multdiv;
+
+// alu related
+logic imdt_sel, alu_src;
+logic[1:0] alu_op;
+
+// PC address related
+logic branch, jump, jumpreg;
+
 
     initial begin
+        reset = 1;
+        #1;
+        assert (read == 1);
+        assert (byteenable == 4'b1111);
+        #1;
+
+        // FETCH
+        reset = 0;
+        state = 0;
+        #1;
+        assert (read == 1);
+        assert (write == 0);
+        assert (byteenable == 4'b1111);
+        assert (reg_write_enable == 0);
+        #1;
+
+        // MEM
+        state = 2;
+        opcode = 40;
+        byte_addressing = 2'b00;
+        #1;
+        assert (read == 0);
+        assert (write == 1);
+        assert (byteenable == 4'b0001);
+        assert (write_data_sel == 1);
+        assert (alu_op == 2'b00);
+        assert (alu_src == 1);
+        #1;
+        byte_addressing = 2'b01;
+        #1;
+        assert (read == 0);
+        assert (write == 1);
+        assert (byteenable == 4'b0010);
+        assert (write_data_sel == 2);
+        assert (alu_op == 2'b00);
+        assert (alu_src == 1);
+        #1;
+        byte_addressing = 2'b10;
+        #1;
+        assert (read == 0);
+        assert (write == 1);
+        assert (byteenable == 4'b0100);
+        assert (write_data_sel == 3);
+        assert (alu_op == 2'b00);
+        assert (alu_src == 1);
+        #1;
+        byte_addressing = 2'b11;
+        #1;
+        assert (read == 0);
+        assert (write == 1);
+        assert (byteenable == 4'b1000);
+        assert (write_data_sel == 4);
+        assert (alu_op == 2'b00);
+        assert (alu_src == 1);
+        #1;
+        opcode = 41;
+        byte_addressing = 2'b00;
+        #1;
+        assert (read == 0);
+        assert (write == 1);
+        assert (byteenable == 4'b0011);
+        assert (write_data_sel == 5);
+        assert (alu_op == 2'b00);
+        assert (alu_src == 1);
+        #1;
+        byte_addressing = 2'b10;
+        #1;
+        assert (read == 0);
+        assert (write == 1);
+        assert (byteenable == 4'b1100);
+        assert (write_data_sel == 6);
+        assert (alu_op == 2'b00);
+        assert (alu_src == 1);
+        #1;
+        opcode = 43;
+        #1;
+        assert (read == 0);
+        assert (write == 1);
+        assert (byteenable == 4'b1111);
+        assert (alu_op == 2'b00);
+        assert (alu_src == 1);
+        #1;
+        opcode = 32;
+        #1;
+        assert (read == 1);
+        assert (write == 0);
+        assert (byteenable == 4'b1111);
+        assert (alu_op == 2'b00);
+        assert (alu_src == 1);
+        #1;
+        opcode = 33;
+        #1;
+        assert (read == 1);
+        assert (write == 0);
+        assert (byteenable == 4'b1111);
+        assert (alu_op == 2'b00);
+        assert (alu_src == 1);
+        #1;
+        opcode = 34;
+        #1;
+        assert (read == 1);
+        assert (write == 0);
+        assert (byteenable == 4'b1111);
+        assert (alu_op == 2'b00);
+        assert (alu_src == 1);
+        #1;
+        opcode = 35;
+        #1;
+        assert (read == 1);
+        assert (write == 0);
+        assert (byteenable == 4'b1111);
+        assert (alu_op == 2'b00);
+        assert (alu_src == 1);
+        #1;
+        opcode = 36;
+        #1;
+        assert (read == 1);
+        assert (write == 0);
+        assert (byteenable == 4'b1111);
+        assert (alu_op == 2'b00);
+        assert (alu_src == 1);
+        #1;
+        opcode = 37;
+        #1;
+        assert (read == 1);
+        assert (write == 0);
+        assert (byteenable == 4'b1111);
+        assert (alu_op == 2'b00);
+        assert (alu_src == 1);
+        #1;
+        opcode = 38;
+        #1;
+        assert (read == 1);
+        assert (write == 0);
+        assert (byteenable == 4'b1111);
+        assert (alu_op == 2'b00);
+        assert (alu_src == 1);
+        #1;
+        opcode = 0;
+        #1;
+        assert (read == 0);
+        assert (write == 0);
+        #1;
+
+
+
+        state = 3;
         //R Type
         opcode = 0;
 
@@ -83,8 +240,8 @@ module control_tb(
         opcode = 2;
         #1;
         // $display("a=%d, b=%d, r=%d, time=%t", A, B, alu_out, $time);
-        assert(jump1 == 1) else $fatal(1, "Jump, mux does not choose jump address, chooses branch output instead");
-        assert(jump2 == 0) else $fatal(1, "Jump, mux does not choose jump address, chooses read_data_b from reg file instead");
+        assert(jump == 1) else $fatal(1, "Jump, mux does not choose jump address, chooses branch output instead");
+        assert(jumpreg == 0) else $fatal(1, "Jump, mux does not choose jump address, chooses read_data_b from reg file instead");
 
         #1
 
@@ -94,10 +251,10 @@ module control_tb(
         function_code = 9;
         #1;
         // $display("a=%d, b=%d, r=%d, time=%t", A, B, alu_out, $time);
-        assert(jump2 == 1) else $fatal(1, "JALR, mux does not choose read_data_b from reg file, chooses jump address instead");
+        assert(jumpreg == 1) else $fatal(1, "JALR, mux does not choose read_data_b from reg file, chooses jump address instead");
         assert(rd_select == 1) else $fatal(1, "JALR does not select correct register to write");
         assert(reg_write_enable == 1)  else $fatal(1, "JALR cannot save data in reg file");
-        assert(data_into_reg2 == 1) else $fatal(1, "JALR does not store current pc+4 at the register");
+        assert(link_to_reg == 1) else $fatal(1, "JALR does not store current pc+4 at the register");
         #1
         //JR
 
@@ -108,7 +265,7 @@ module control_tb(
         #1;
         // $display("a=%d, b=%d, r=%d, time=%t", A, B, alu_out, $time);
         assert(branch == 1) else $fatal(1, "BEQ does not give high signal to branch");
-        assert((jump1 == 0) & (jump2 == 0)) else $fatal(1, "BEQ does not send correct mux signal to give branch address to pc");
+        assert((jump == 0) & (jumpreg == 0)) else $fatal(1, "BEQ does not send correct mux signal to give branch address to pc");
         assert(reg_write_enable == 0)  else $fatal(1, "BEQ cannot save data in reg file");
         assert(alu_src == 0) else $fatal(1, "BEQ does not do subtraction correctly");
         assert(alu_op == 1) else $fatal(1, "BEQ does not send correct alu op");
@@ -121,44 +278,53 @@ module control_tb(
         #1;
         // $display("a=%d, b=%d, r=%d, time=%t", A, B, alu_out, $time);
         assert(branch == 1) else $fatal(1, "BLTZAL does not give high signal to branch");
-        assert((jump1 == 0) & (jump2 == 0)) else $fatal(1, "BLTZAL does not send correct mux signal to give branch address to pc");
-        assert(reg_write_enable == 0)  else $fatal(1, "BLTZAL cannot save data in reg file");
+        assert((jump == 0) & (jumpreg == 0)) else $fatal(1, "BLTZAL does not send correct mux signal to give branch address to pc");
+        assert(reg_write_enable == 1)  else $fatal(1, "BLTZAL cannot save data in reg file");
         assert(alu_src == 1) else $fatal(1, "BLTZAL does not do subtraction correctly");
-        assert(data_into_reg2 == 1) else $fatal(1, "BLTZAL does not store pc + 4 correctly");
+        assert(link_to_reg == 1) else $fatal(1, "BLTZAL does not store pc + 4 correctly");
         #1
 
         //lui
         #1;
         opcode = 15;
         #1;
-        // $display("data_into_reg1=%b", data_into_reg1);
-        // $display("data_into_reg2=%b", data_into_reg2);
+        // $display("datamem_to_reg=%b", datamem_to_reg);
+        // $display("link_to_reg=%b", link_to_reg);
         assert(alu_src == 1) else $fatal(1, "LUI does not choose correct input to ALU");
-        assert(data_into_reg1 == 0) else $fatal(1, "LUI does not choose correct data1 to write into destination register");
-        assert(data_into_reg2 == 0) else $fatal(1, "LUI does not choose correct data2 to write into destination register");
+        assert(datamem_to_reg == 0) else $fatal(1, "LUI does not choose correct data1 to write into destination register");
+        assert(link_to_reg == 0) else $fatal(1, "LUI does not choose correct data2 to write into destination register");
 
         $display("Finished. Total time = %t", $time);
         $finish;
     end
 
     control dut(
-        .opcode(opcode),
-        .function_code(function_code),
-        .b_code(b_code),
-        .rd_select(rd_select),
-        .imdt_sel(imdt_sel),
-        .branch(branch),
-        .jump1(jump1),
-        .jump2(jump2),
-        .alu_op(alu_op),
-        .alu_src(alu_src),
-        .data_read(data_read),
-        .data_write(data_write),
-        .reg_write_enable(reg_write_enable),
-        .hi_wren(hi_wren),
-        .lo_wren(lo_wren),
-        .data_into_reg1(data_into_reg1),
-        .data_into_reg2(data_into_reg2)
+      // inputs
+      .reset(reset), .opcode(opcode), .function_code(function_code), .b_code(b_code),
+      .state(state), .waitrequest(waitrequest), .byte_addressing(byte_addressing),
+      // memory related
+      .read(read), .write(write),             // read and write enable signals to bus_memory.v\
+      .write_data_sel(write_data_sel),        // control signal to writedata_selector.v, for store instructions
+      .byteenable(byteenable),                // control signal to bus_memory.v for sb and sh instructions
+      // register_file related
+      .rd_select(rd_select),                  // select signal to destination_reg_selector.v to select destination register
+      .reg_write_enable(reg_write_enable),    // write enable signal for register in register_file.v
+      .datamem_to_reg(datamem_to_reg),        // control signal to reg_writedata_selector.v, for load instructions
+      .link_to_reg(link_to_reg),              // control signal to reg_writedata_selector.v, for link instructions
+      .mfhi(mfhi), .mflo(mflo),               // control signal to reg_writedata_selector.v, for mfhi and mflo instructions
+      .lwl(lwl), .lwr(lwr),                   // control signals to register_file.v for lwl and lwr instructions
+      // hi and lo registers related
+      .hi_wren(hi_wren),                      // write enable signal for reg_hi.v, for mthi, multiplication and division instructions
+      .lo_wren(lo_wren),                      // write enable signal for reg_lo.v, for mtlo, multiplication and division instructions
+      .multdiv(multdiv),                      // control signal to reg_hi.v and reg_lo.v, for multiplication and divison instructions
+      // alu related
+      .imdt_sel(imdt_sel),                    // control signal to imdtmux, selects between sign extended or zero extended immediate for andi, ori, xori
+      .alu_src(alu_src),                      // control signal to alumux, selects between read_data_b for R-type instructions or immdt_32 for I-type instructions
+      .alu_op(alu_op),                        // control signal to alu_ctrl.v, 0 for load/store instructions, 1 for BEQ/BNE, 2 for R-type instructions, 3 for I-type instructions
+      // PC address related
+      .branch(branch),                        // control signal to branch_cond.v, for branch instructions
+      .jump(jump),                            // control signal to PC_address_selector.v for J and JAL
+      .jumpreg(jumpreg)                       // control signal to PC_address_selector.v for JR and JALR
     );
 
 endmodule
