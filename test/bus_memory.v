e2318e7197202839b407e1892cefcd4e68bae0ab
sytan98 <tan.siyu@dhs.sg>
2020-12-18 23:39:23 +0800

    updated bus memory comments

--- test/bus_memory.v
+++ test/bus_memory.v
@@ -9,18 +9,17 @@ module bus_memory(
     input logic[3:0] byteenable,
     output logic[31:0] readdata
 );
-	// Memory (256 x 4 x 8-bit bytes) address is 32 bits (used only 10), word is 32 bits
-	// Full memory supposed to be able to store 2^30 words but we reduce it to store 2^8 (256) words.
-	// 10 bits used for address as it is byte addressing.
+	// Memory (256 x 4 x 8-bit bytes) address is 32 bits (used only 11), word is 32 bits
+	// Full memory supposed to be able to store 2^30 words but we reduce it to store 2^9 (512) words.
+	// 11 bits used for address as it is byte addressing.
+
 	parameter ROM_INIT_FILE = "";
 	parameter RAM_INIT_FILE = "";
 	parameter address_bit_size = 11;
-	parameter reset_vector = 128; // this is word addressing, technically should be 0xbfc00000
+	parameter reset_vector = 128; // this is word addressing. 128 * 4 = 32'h00000200 which is mapped to 0xbfc00000
 
 	logic[3:0] num_stall_reg;
 	// instructions start at word 128
-	// this divides memory equally between data(first half excluding address 0) and instructions (second half starting at word 128)
-
 
 	// If address > BFC00000, it is instruction address and needs to be mapped to start at word 128
 	// otherwise, it is a data address (keep the same)
@@ -35,7 +34,7 @@ module bus_memory(
 
 	//mem state
 	typedef enum logic[1:0] {
-			IDLE = 2'b00,
+			IDLE = 2'b00, //
 			BUSY = 2'b01,
 			CHILL = 2'b10
 	} state_t;
