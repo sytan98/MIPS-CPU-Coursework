90e1c6a6060d0a3f07464bdfd093d6e05ed650d9
sytan98 <tan.siyu@dhs.sg>
2020-12-21 20:05:10 +0800

    delete comments

--- rtl/mips_cpu/data_register.v
+++ rtl/mips_cpu/data_register.v
@@ -9,23 +9,7 @@ module data_register(
 );
   logic[31:0] dr;
   logic waitreg;
-  //logic regcheck;
   assign dr_readdata = dr;
-
-  // always_ff @(posedge clk) begin
-  //   waitreg <= waitrequest;
-  //   if(reset) begin
-  //     dr <= 0;
-  //   end
-  //   // else if ( (state == 2 & waitrequest == 0) | (state == 3 & dr_writedata!=32'hxxxxxxxx) )begin
-  //   else if ( (state == 2 & waitreg == 1 & waitrequest == 0 ) | (state == 3 & waitreg == 0 & waitrequest==0 & regcheck!=1)  )  begin
-  //       regcheck <= 1;
-  //       dr <= dr_writedata;            //load the DR during MEM state and when waitrequest == 0
-  //   end
-  //   if (state == 0) begin
-  //     regcheck <= 0;
-  //   end
-  // end
 
   always_ff @(posedge clk) begin
     if(reset) begin
